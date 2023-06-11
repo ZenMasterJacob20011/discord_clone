47f2c010a8c3b5f200beb526133a9788ffe71bcc
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2023-06-10 19:57:00 -0500

    Added username to message data

--- src/main/java/com/example/json/MessageJSON.java
+++ src/main/java/com/example/json/MessageJSON.java
@@ -14,12 +14,21 @@ public class MessageJSON {
 
     private String message;
 
+    private String username;
     public String getMessage() {
         return message;
     }
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Integer getId() {
