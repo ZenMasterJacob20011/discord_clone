f5767c2ce7c903b71fd0325315ab52430a6fd06b
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2023-06-10 20:00:57 -0500

    Displays the logged-in user message on left and other user messages on right

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,6 @@
 
 function sendMessage() {
+    var curUser = document.cookie.split("=")[1];
     const input = document.querySelector("#typedinput").value;
     fetch('http://localhost:8080/postmessages', {
         method: 'POST',
@@ -11,6 +12,16 @@ function sendMessage() {
     }).then(response => response.json())
         .then(response => console.log(JSON.stringify(response)))
     document.querySelector("#typedinput").value = '';
+    const messageBox = document.querySelector("#messages");
+    const divMessage = document.createElement("div");
+    const tempMessage = document.createElement("p");
+    const tempUserMessage = document.createElement("p");
+    divMessage.classList.add("d-flex", "flex-row", "justify-content-start", "mb-4");
+    tempMessage.classList.add("small","p-2","me-3","mb-1","text-white","rounded-3","bg-primary");
+    tempUserMessage.textContent = curUser;
+    tempMessage.textContent = input;
+    divMessage.appendChild(tempMessage);
+    messageBox.appendChild(divMessage);
 }
 
 async function grabAllMessages() {
@@ -24,10 +35,20 @@ async function grabAllMessages() {
         }
     });
     messages = await response.json();
-    for (const message of messages) {
+    console.log(messages);
+    var curUser = document.cookie.split("=")[1];
+    console.log(curUser);
+    for (const curMessage of messages) {
+        console.log(curMessage);
+        let message = curMessage.message;
+        let username = curMessage.username;
         const divMessage = document.createElement("div");
         const tempMessage = document.createElement("p");
-        divMessage.classList.add("d-flex","flex-row","justify-content-start","mb-4");
+        if(curUser === username) {
+            divMessage.classList.add("d-flex", "flex-row", "justify-content-start", "mb-4");
+        }else{
+            divMessage.classList.add("d-flex", "flex-row", "justify-content-end", "mb-4")
+        }
         tempMessage.classList.add("small","p-2","me-3","mb-1","text-white","rounded-3","bg-primary");
         tempMessage.textContent = message;
         divMessage.appendChild(tempMessage);
