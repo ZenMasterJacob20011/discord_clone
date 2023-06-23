09b0ab4e88d2e845337cdd8fb5102601246a65af
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2023-06-22 23:53:42 -0500

    Added websocket methods for chat

--- src/main/java/com/example/controller/ChatController.java
+++ src/main/java/com/example/controller/ChatController.java
@@ -9,6 +9,9 @@ import com.example.util.JWTService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +24,8 @@ public class ChatController {
 
     @Autowired
     private DatabaseUtil databaseUtil;
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     @GetMapping("/getmessages")
     @ResponseBody
@@ -30,15 +35,25 @@ public class ChatController {
 
 
     @PostMapping(value = "/postmessages",consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<?> addMessageToDatabase(@RequestBody MessageJSON input, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization){
+    public ResponseEntity<?> addMessageToDatabase(@RequestBody MessageJSON input, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) throws InterruptedException {
         JWTService jwtService = new JWTService();
         if(databaseUtil.containsJWT(authorization)) {
             String username = (String) jwtService.decodeJWT(authorization).get("sub");
             input.setUsername(username);
             databaseUtil.saveMessage(input);
+            simpMessagingTemplate.convertAndSend("/topic/chat",input);
             return ResponseEntity.ok(input);
         }
         return new ResponseEntity<>("Invalid auth token", HttpStatus.FORBIDDEN);
     }
+
+
+    @SendTo("/topic/chat")
+    public MessageJSON messageHandler(MessageJSON messageJSON) throws InterruptedException {
+        System.out.println(messageJSON);
+        Thread.sleep(1000);
+        return messageJSON;
+    }
+
 
 }
