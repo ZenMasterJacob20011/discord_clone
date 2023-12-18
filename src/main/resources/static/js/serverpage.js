b93e9a580d30876d4d6761195ab676582afd19b2
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2023-12-17 18:22:12 -0600

    addMessage now handles messages posted by same user in less than 7 min

--- src/main/resources/static/js/serverpage.js
+++ src/main/resources/static/js/serverpage.js
@@ -1,7 +1,7 @@
 import {
     decodedJWTJSON, getServerInformationByID,
     handleCopyInviteLinkButton,
-    loadNameTag, Message
+    loadNameTag, Message, MessageWithProfilePicture
 } from "./util.js"
 import {handleContextMenu, loadSideServers} from "./serversidebar.js";
 
@@ -14,7 +14,7 @@ handleCopyInviteLinkButton()
 
 async function sendMessage(serverID) {
     const input = document.querySelector("#typedinput").value;
-    const response = await fetch(`http://localhost:8080/server/postmessages/${serverID}`, {
+    const response = await fetch(`http://localhost:8080/server/${serverID}/postmessages`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -33,8 +33,9 @@ async function sendMessage(serverID) {
     }
 }
 
+
 export async function loadMessages(server_id) {
-    fetch(`http://localhost:8080/server/messages/${server_id}`, {
+    fetch(`http://localhost:8080/server/${server_id}/messages`, {
         method: "GET"
     }).then((response) => {
         if (!response.ok) {
@@ -53,9 +54,29 @@ export async function loadMessages(server_id) {
 
 export function addMessage(jsonData) {
     console.log(jsonData)
-    const curUser = decodedJWTJSON.sub;
+    let previousMessageHTML = null
+    let previousTimestamp = null
+    let previousUsername = null
+    if(document.getElementById("main-content-mid").hasChildNodes()) {
+        previousMessageHTML = document.getElementById("main-content-mid").lastElementChild;
+        previousTimestamp = previousMessageHTML.attributes.getNamedItem("data-timestamp").nodeValue;
+        previousUsername = previousMessageHTML.attributes.getNamedItem("data-username").nodeValue;
+    }
+    console.log(previousMessageHTML);
+    const hasSevenMinutesPassed = () => {
+        const previousDatePlusSeven = new Date(previousTimestamp).addMinutes(7);
+        const currentDate = new Date(jsonData.postTime);
+        return currentDate >= previousDatePlusSeven;
+
+    }
     if (window.location.pathname.match("\\/\\d+|\\/@me")[0].substring(1) == jsonData.serverID) {
-        document.getElementById("main-content-mid").innerHTML += Message(jsonData.username === curUser, jsonData.message, jsonData.username);
+        if(previousMessageHTML===null){
+            document.getElementById("main-content-mid").innerHTML += MessageWithProfilePicture(jsonData.message, jsonData.username, jsonData.postTime);
+        }else if(jsonData.username === previousUsername && !hasSevenMinutesPassed()) {
+            document.getElementById("main-content-mid").innerHTML += Message(jsonData.message, jsonData.username, jsonData.postTime);
+        }else{
+            document.getElementById("main-content-mid").innerHTML += MessageWithProfilePicture(jsonData.message, jsonData.username, jsonData.postTime);
+        }
     }
 }
 
@@ -93,7 +114,7 @@ function ServerOptionsMenu(server_id) {
 
 function ServerTopBar() {
     return `
-        <div class="container-fluid d-flex justify-content-between align-items-center">
+        <div class="h-100 container-fluid d-flex justify-content-between align-items-center">
             <div class="flex-container">
                 <svg width="24" height="24" viewBox="0 0 24 24" class="icon" x="0" y="0" aria-hidden="true" role="img">
                     <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M5.88657 21C5.57547 21 5.3399 20.7189 5.39427 20.4126L6.00001 17H2.59511C2.28449 17 2.04905 16.7198 2.10259 16.4138L2.27759 15.4138C2.31946 15.1746 2.52722 15 2.77011 15H6.35001L7.41001 9H4.00511C3.69449 9 3.45905 8.71977 3.51259 8.41381L3.68759 7.41381C3.72946 7.17456 3.93722 7 4.18011 7H7.76001L8.39677 3.41262C8.43914 3.17391 8.64664 3 8.88907 3H9.87344C10.1845 3 10.4201 3.28107 10.3657 3.58738L9.76001 7H15.76L16.3968 3.41262C16.4391 3.17391 16.6466 3 16.8891 3H17.8734C18.1845 3 18.4201 3.28107 18.3657 3.58738L17.76 7H21.1649C21.4755 7 21.711 7.28023 21.6574 7.58619L21.4824 8.58619C21.4406 8.82544 21.2328 9 20.9899 9H17.41L16.35 15H19.7549C20.0655 15 20.301 15.2802 20.2474 15.5862L20.0724 16.5862C20.0306 16.8254 19.8228 17 19.5799 17H16L15.3632 20.5874C15.3209 20.8261 15.1134 21 14.8709 21H13.8866C13.5755 21 13.3399 20.7189 13.3943 20.4126L14 17H8.00001L7.36325 20.5874C7.32088 20.8261 7.11337 21 6.87094 21H5.88657ZM9.41045 9L8.35045 15H14.3504L15.4104 9H9.41045Z"></path>
