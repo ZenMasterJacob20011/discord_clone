7fc6f206ce8c7f6599147c757c99d4d1d9b90d57
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2023-12-20 19:58:51 -0600

    getServerInformationByID will now cache the server in local storage

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -15,10 +15,21 @@ function parseJwt(token) {
 
 export function Circle(width, height) {
     return `
-        <div style="width: ${width}; height: ${height}" class="circle">
+        <div style="width: ${width}; height: ${height}; position: absolute" class="circle">
             
         </div>
     `
+}
+
+export function SearchBar() {
+    return `
+        <div class="search-bar rounded">
+            <input class="search p-2" name="search-for-friends" type="text" placeholder="Search">
+            <svg class="icon me-2" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 14.242 14.243C13.109 15.376 11.603 16 10 16Z"></path>
+            </svg>
+        </div>
+    `;
 }
 
 /**
@@ -133,32 +144,43 @@ window.onload = function () {
     loadNameTag();
 }
 
-async function getInviteLink(serverID) {
-    let response = await fetch(`http://localhost:8080/server/${serverID}/invites`, {
+export async function getInviteLink(serverID) {
+    return fetch(`http://localhost:8080/invite/getInviteLink/${serverID}`, {
         method: "GET",
         headers: {
             "content-type": "application/json",
             "authorization": jwt
         }
+    }).then(response => {
+        if (response.ok){
+            return response.json();
+        }
+        throw Error("Could not get response json for invite link")
+    }).then(responseJSON => {
+        return responseJSON;
     });
-    let responseJSON = await response.json();
-    let inviteCode = responseJSON.invite.inviteCode;
-    let inviter = responseJSON.invite.inviter;
-    $("#inviteCode").text(`http://localhost:8080/invite/${inviteCode}`);
 }
 
 /**
- * returns server information given a server_id
- * @param server_id {Number} the id of the server e.g. 1
- * @returns {Object}
+ * returns server information given a server_id as a JSON object
+ * @param server_id {String} the id of the server e.g. 1
+ * @returns {any} The JSON object containing server information
  */
 export async function getServerInformationByID(server_id) {
-    const response = await fetch(`http://localhost:8080/server/${server_id}/getServerInfo`, {
-        method: "GET"
-    });
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw Error(await response.text());
+    if (localStorage.getItem(server_id) === null){
+        return fetch(`http://localhost:8080/server/${server_id}/getServerInfo`, {
+            method: "GET"
+        }).then(r => {
+            if (!r.ok){
+                throw Error(`Could not fetch server info with the id ${server_id}`);
+            }
+            r.json().then(serverInfo => {
+                console.log(serverInfo)
+                localStorage.setItem(String(server_id),JSON.stringify(serverInfo));
+                return serverInfo;
+            })
+        });
     }
+    return JSON.parse(localStorage.getItem(server_id));
+
 }
