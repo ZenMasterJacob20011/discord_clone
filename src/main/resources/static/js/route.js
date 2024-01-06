d0fde12fe0f0b27fb4e735ee096c09c8eb85cc2d
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-01-05 21:09:51 -0600

    Routes can now handle channel navigation

--- src/main/resources/static/js/route.js
+++ src/main/resources/static/js/route.js
@@ -1,23 +1,54 @@
 import {loadProfilePage} from "./profilepage.js";
-import {loadServerPage} from "./serverpage.js";
+import {loadChannel, loadServerPage} from "./serverpage.js";
+import {getServerInformationByID} from "./util.js";
 
+let currentServerID = null;
+
+
+/**
+ * This will update the page accordingly to the current href in the browsers' path. Ideally it is not to be used directly
+ * but instead to be used within another function
+ * @returns {Promise<void>}
+ */
 const router = async () => {
     //if the href is just the server then default the general channel
     //if the href is the server and channel then add logic for channel navigation
-    const href = window.location.pathname.match("\\/\\d+|\\/@me")[0].substring(1);
-    if(href === null){
+    const re = /\/\d+|\/@me/g
+    const href = window.location.pathname.match(re);
+    console.log("href: " + href);
+    if (href === null) {
         return;
     }
     console.log(href)
-    if(href === "@me"){
-        loadProfilePage();
-    }else{
-        loadServerPage(href);
+    if (href.length === 1) {
+        if (href[0].substring(1) === "@me") {
+            loadProfilePage();
+        } else {
+            const serverID = href[0].substring(1);
+            const serverInfo = await getServerInformationByID(serverID)
+            loadServerPage(serverInfo);
+            loadChannel(serverInfo.channels[0].channelID);
+        }
+    } else if (href.length > 1) {
+        const serverID = href[0].substring(1);
+        const channelID = href[1].substring(1);
+        if (currentServerID == serverID){
+            loadChannel(channelID);
+        }else{
+            const serverInfo = await getServerInformationByID(serverID)
+            loadServerPage(serverInfo);
+            loadChannel(channelID);
+        }
     }
+    currentServerID = href[0].substring(1);
 }
 
+/**
+ * used to navigate to another page. will push the state of the page to the history api for forward and backward traversal
+ * @param href {String} the page to navigate to
+ */
 export function navigateTo(href) {
-    history.pushState(null, null, `${href}`);
+    history.pushState(null, null, `http://localhost:8080/server/${href}`);
     router();
 }
 
