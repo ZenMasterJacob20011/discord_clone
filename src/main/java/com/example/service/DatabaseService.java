5dd88a8180710785e61429020acefffefb759c5e
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-01-14 01:42:19 -0600

    Added getUsersChannelsByJWT and getDirectMessageChannelByUsers functions

--- src/main/java/com/example/service/DatabaseService.java
+++ src/main/java/com/example/service/DatabaseService.java
@@ -11,8 +11,6 @@ import java.util.Optional;
 
 @Service
 public class DatabaseService {
-
-
     @Autowired
     private MessageRepository messageRepository;
     @Autowired
@@ -23,6 +21,7 @@ public class DatabaseService {
     private InviteRepository inviteRepository;
     @Autowired
     private ChannelRepository channelRepository;
+
     public boolean isValidUser(String userName, String password) {
         return userRepository.existsByUsernameAndPassword(userName, password);
     }
@@ -30,7 +29,8 @@ public class DatabaseService {
     public boolean isValidUser(User user) {
         return isValidUser(user.getUsername(), user.getPassword());
     }
-    public boolean doesUserExist(String username){
+
+    public boolean doesUserExist(String username) {
         return userRepository.existsByUsername(username);
     }
 
@@ -39,24 +39,25 @@ public class DatabaseService {
         userRepository.save(user);
     }
 
-    public User getUser(String userName){
-        return userRepository.findPersonIdentifierByUsername(userName);
+    public User getUser(String userName) {
+        return userRepository.getUserByUsername(userName);
     }
 
     public User getUserByJWT(String JWT) throws Exception {
-        if (userRepository.findPersonIdentifierByJWT(JWT).isPresent()) {
-            return userRepository.findPersonIdentifierByJWT(JWT).get();
+        if (userRepository.findUserByJWT(JWT).isPresent()) {
+            return userRepository.findUserByJWT(JWT).get();
         }
         throw new Exception("could not find user with JWT: " + JWT);
     }
-    public void addJWTForUser(User user, String JWT){
-        User oldPI = userRepository.findPersonIdentifierByUsername(user.getUsername());
+
+    public void addJWTForUser(User user, String JWT) {
+        User oldPI = userRepository.getUserByUsername(user.getUsername());
         oldPI.setJWT(JWT);
         userRepository.save(oldPI);
     }
 
     public List<Message> getMessagesByChannelID(int channelID) throws Exception {
-        if(messageRepository.findMessagesByChannel_ChannelID(channelID).isPresent()) {
+        if (messageRepository.findMessagesByChannel_ChannelID(channelID).isPresent()) {
             return messageRepository.findMessagesByChannel_ChannelID(channelID).get();
         }
         throw new Exception("Could not find channel with ID: " + channelID);
@@ -65,40 +66,47 @@ public class DatabaseService {
     public boolean containsJWT(String JWT) {
         return userRepository.existsByJWT(JWT);
     }
-    public boolean isValidJWT(String JWT){
+
+    public boolean isValidJWT(String JWT) {
         return containsJWT(JWT);
     }
 
-    public void saveServer(Server server){
+    public void saveServer(Server server) {
         serverRepository.save(server);
     }
-    public void deleteServer(Integer serverID){
+
+    public void deleteServer(Integer serverID) {
         serverRepository.deleteById(serverID);
     }
-    public void deleteServer(Server server){
+
+    public void deleteServer(Server server) {
         deleteServer(server.getServerID());
     }
-    public Optional<Server> findServerByServerID(Integer serverID){
+
+    public Optional<Server> findServerByServerID(Integer serverID) {
         return serverRepository.findById(serverID);
     }
-    public Optional<Server> getServerByInviteCode(String inviteCode){
+
+    public Optional<Server> getServerByInviteCode(String inviteCode) {
         return serverRepository.findServerByInvites_InviteCode(inviteCode);
     }
+
     public void addServerToUser(Integer serverID, String userName) throws Exception {
         Server server = findServerByServerID(serverID).get();
         User user = getUser(userName);
-        if (user.getServerList().contains(server)){
+        if (user.getServerList().contains(server)) {
             throw new Exception(userName + " is already a member of " + server.getServerName());
         }
         user.addServer(server);
         userRepository.save(user);
     }
-    public void addMessage(Message message){
+
+    public void addMessage(Message message) {
         messageRepository.save(message);
     }
 
     public Invite getInviteByInviteCode(String inviteCode) throws Exception {
-        if(inviteRepository.findInviteByInviteCode(inviteCode).isPresent()) {
+        if (inviteRepository.findInviteByInviteCode(inviteCode).isPresent()) {
             return inviteRepository.findInviteByInviteCode(inviteCode).get();
         }
         throw new Exception("Could not find invite with the invite code: " + inviteCode);
@@ -112,7 +120,7 @@ public class DatabaseService {
     }
 
     public Server createServer(Server server) {
-        if(server.getChannels().isEmpty()){
+        if (server.getChannels().isEmpty()) {
             ArrayList<Channel> tempChannels = new ArrayList<>();
             Channel tempChannel = new Channel();
             tempChannel.setServer(server);
@@ -125,8 +133,8 @@ public class DatabaseService {
         return server;
     }
 
-    public void generateInvite(Integer serverID,String inviter) throws Exception {
-        if(findServerByServerID(serverID).isPresent()){
+    public void generateInvite(Integer serverID, String inviter) throws Exception {
+        if (findServerByServerID(serverID).isPresent()) {
             Server server = findServerByServerID(serverID).get();
             Invite invite = new Invite(inviter);
             invite.setServer(server);
@@ -139,7 +147,7 @@ public class DatabaseService {
         throw new Exception("Server with ID (" + serverID + ") not found");
     }
 
-    public void createChannel(Integer serverID,String channelName) throws Exception {
+    public void createChannel(Integer serverID, String channelName) throws Exception {
         if (serverRepository.findById(serverID).isPresent()) {
             Server tempServer = serverRepository.findById(serverID).get();
             Channel tempChannel = new Channel();
@@ -149,5 +157,31 @@ public class DatabaseService {
             return;
         }
         throw new Exception("Server with ID (" + serverID + ") not found");
+    }
+
+    public List<Channel> getUsersChannelsByJWT(String jwt) {
+        if (channelRepository.findChannelsByJWT(jwt).isPresent()) {
+            return channelRepository.findChannelsByJWT(jwt).get();
+        }
+        return new ArrayList<>();
+    }
+
+    public Channel getDirectMessageChannelByUsers(String userOne, String userTwo) {
+        if (channelRepository.findDirectMessageChannelByUsers(userOne, userTwo).isPresent()) {
+            return channelRepository.findDirectMessageChannelByUsers(userOne, userTwo).get();
+        }
+        User userOneObject = userRepository.getUserByUsername(userOne);
+        User userTwoObject = userRepository.getUserByUsername(userTwo);
+        Server atMeServer = new Server();
+        Channel channel = new Channel();
+        channel.setServer(atMeServer);
+        atMeServer.setServerName("@me");
+        atMeServer.addUserToServer(userOneObject);
+        atMeServer.addUserToServer(userTwoObject);
+        atMeServer.getChannels().add(channel);
+        userOneObject.addServer(atMeServer);
+        userTwoObject.addServer(atMeServer);
+        serverRepository.save(atMeServer);
+        return getDirectMessageChannelByUsers(userOne, userTwo);
     }
 }
